abff8d4aab081f5403fd3897e976357749a0fe87
Mik <m.shac86@gmail.com>
2022-07-03 19:03:56 +0300

    HW_06 different implementations

--- src/telran/text/Strings.java
+++ src/telran/text/Strings.java
@@ -45,6 +45,7 @@ public class Strings {
 		if (str1.length() != str2.length() || str1.length() <= 0) {
 			return false;
 		}
+	/** The first implementation (case insensitive)**/	
 		str1 = str1.toLowerCase();
 		str2 = str2.toLowerCase();
 		int[] ar = new int[26];
@@ -53,6 +54,30 @@ public class Strings {
 			ar[str1.charAt(i) - 97] += 1;
 			arCompar[str2.charAt(i) - 97] += 1;
 		}
+	/** The second implementation (case sensitive)**/	
+//		int[] ar = new int[52];
+//		int[] arCompar = new int[52];
+//		for(int i = 0; i < str1.length(); i++) {
+//			char symbStr1 = str1.charAt(i);
+//			char symbStr2 = str2.charAt(i);
+//			if(symbStr1 < 91) {
+//				ar[symbStr1 - 65] += 1;
+//			} else {
+//				ar[symbStr1 - 71] += 1;	
+//			}
+//			if(symbStr2 < 91) {
+//				arCompar[symbStr2 - 65] += 1;
+//			} else {
+//				arCompar[symbStr2 - 71] += 1;	
+//			}
+//		}
+	/** The third implementation (case sensitive)**/
+//		int[] ar = new int[63];
+//		int[] arCompar = new int[63];
+//		for(int i = 0; i < str1.length(); i++) {
+//				ar[str1.charAt(i) - 62] += 1;
+//				arCompar[str2.charAt(i) - 62] += 1;	
+//		}
 		return Arrays.equals(ar, arCompar) ? true : false;		
 	}
 }
